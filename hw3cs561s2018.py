20e11d23bb94e92b021d1ca31e8eaaf695bdf008
liyiran <liy@worldticket.net>
2018-04-11 09:33:00 -0700

    initial matrix correct

--- hw3cs561s2018.py
+++ hw3cs561s2018.py
@@ -6,7 +6,7 @@ import time
 
 
 class MDP:
-    def policy_evaluation(self, action):
+    def policy_evaluation(self):
         up_down_walk = np.zeros((1, self.width))
         left_right_walk = np.zeros((self.length, 1))
 
@@ -14,33 +14,37 @@ class MDP:
         left_right_run = np.zeros((self.length, 2))
         new_map = None
         sum = None
-        if action == self.walk_up:
-            new_map = np.delete(np.concatenate((self.value[0:1, ...], self.value), axis=0), -1, axis=0)
-            # self.value = np.multiply(self.p[:,:,self.up, action], new_map) + 
-        elif action == self.walk_down:
-            new_map = np.delete(np.concatenate((self.value, self.value[-1:, ...]), axis=0), 0, axis=0)
-
-        elif action == self.walk_left:
-            new_map = np.delete(np.concatenate((self.value[..., 0:1], self.value), axis=1), -1, axis=1)
-
-        elif action == self.walk_right:
-            new_map = np.delete(np.concatenate((self.value, self.value[..., -1:]), axis=1), 0, axis=1)
-
-        elif action == self.run_up:
-            new_map = np.delete(np.delete(np.concatenate((self.value[0:2:, ...], self.value), axis=0), -1, axis=0), -1, axis=0)
-        elif action == self.run_down:
-            new_map = np.delete(np.concatenate((self.value, self.value[-2:, ...]), axis=0), 1, axis=0)
-            new_map = np.delete(new_map, 1, axis=0)
-        elif action == self.run_left:
-            new_map = np.delete(np.concatenate((self.value[..., 0:2], self.value), axis=1), -1, axis=1)
-            new_map = np.delete(new_map, -1, axis=1)
-        elif action == self.run_right:
-            new_map = np.delete(np.concatenate((self.value, self.value[..., -2:]), axis=1), -1, axis=1)
-            new_map = np.delete(new_map, -1, axis=1)
-        return np.multiply(self.p[:, :, self.up, action].flatten().reshape(self.length, self.width), new_map) + \
-               np.multiply(self.p[:, :, self.down, action].flatten().reshape(self.length, self.width), new_map) \
-               + np.multiply(self.p[:, :, self.left, action].flatten().reshape(self.length, self.width), new_map) + \
-               np.multiply(self.p[:, :, self.right, action].flatten().reshape(self.length, self.width), new_map)
+        # if action == self.walk_up:
+        #     new_map = np.delete(np.concatenate((self.value[0:1, ...], self.value), axis=0), -1, axis=0)
+        #     # self.value = np.multiply(self.p[:,:,self.up, action], new_map) + 
+        # elif action == self.walk_down:
+        #     new_map = np.delete(np.concatenate((self.value, self.value[-1:, ...]), axis=0), 0, axis=0)
+        # 
+        # elif action == self.walk_left:
+        #     new_map = np.delete(np.concatenate((self.value[..., 0:1], self.value), axis=1), -1, axis=1)
+        # 
+        # elif action == self.walk_right:
+        #     new_map = np.delete(np.concatenate((self.value, self.value[..., -1:]), axis=1), 0, axis=1)
+        # 
+        # elif action == self.run_up:
+        #     new_map = np.delete(np.delete(np.concatenate((self.value[0:2:, ...], self.value), axis=0), -1, axis=0), -1, axis=0)
+        # elif action == self.run_down:
+        #     new_map = np.delete(np.concatenate((self.value, self.value[-2:, ...]), axis=0), 1, axis=0)
+        #     new_map = np.delete(new_map, 1, axis=0)
+        # elif action == self.run_left:
+        #     new_map = np.delete(np.concatenate((self.value[..., 0:2], self.value), axis=1), -1, axis=1)
+        #     new_map = np.delete(new_map, -1, axis=1)
+        # elif action == self.run_right:
+        #     new_map = np.delete(np.concatenate((self.value, self.value[..., -2:]), axis=1), -1, axis=1)
+        #     new_map = np.delete(new_map, -1, axis=1)
+        # return np.multiply(self.p[:, :, self.up, action].flatten().reshape(self.length, self.width), new_map) + \
+        #        np.multiply(self.p[:, :, self.down, action].flatten().reshape(self.length, self.width), new_map) \
+        #        + np.multiply(self.p[:, :, self.left, action].flatten().reshape(self.length, self.width), new_map) + \
+        #        np.multiply(self.p[:, :, self.right, action].flatten().reshape(self.length, self.width), new_map)
+        big_value = self.value[:, :, np.newaxis, np.newaxis]
+        big_value = np.repeat(big_value, 4, 2)
+        big_value = np.repeat(big_value, 8, 3)
+        return np.einsum("jkxy,jkxy->jky", big_value, self.p)
 
     def value_iteration(self):
         u_p = 0
@@ -51,6 +55,7 @@ class MDP:
             u = u_p
             delta = 0
             max = np.full((self.length, self.width), -10000)
+            expect = self.policy_evaluation()
             for action in range(8):
                 if action < 4:
                     u_p = np.maximum(max, self.policy_evaluation(action)) + self.reward_walk
@@ -66,8 +71,8 @@ class MDP:
         self.length = length
         self.width = width
         self.e = e
-        self.up = 0,
-        self.left = 1,
+        self.up = 0
+        self.left = 1
         self.down = 2
         self.right = 3
 
@@ -88,16 +93,16 @@ class MDP:
         self.p[:, :, self.right, self.walk_up] = .5 * (1 - p_walk)
 
         self.p[:, :, self.right, self.walk_right] = p_walk
-        self.p[:, :, self.up, self.walk_left] = .5 * (1 - p_walk)
-        self.p[:, :, self.down, self.walk_left] = .5 * (1 - p_walk)
+        self.p[:, :, self.up, self.walk_right] = .5 * (1 - p_walk)
+        self.p[:, :, self.down, self.walk_right] = .5 * (1 - p_walk)
 
         self.p[:, :, self.down, self.walk_down] = p_walk
         self.p[:, :, self.left, self.walk_down] = .5 * (1 - p_walk)
         self.p[:, :, self.right, self.walk_down] = .5 * (1 - p_walk)
 
         self.p[:, :, self.left, self.walk_left] = p_walk
-        self.p[:, :, self.up, self.walk_down] = .5 * (1 - p_walk)
-        self.p[:, :, self.down, self.walk_down] = .5 * (1 - p_walk)
+        self.p[:, :, self.up, self.walk_left] = .5 * (1 - p_walk)
+        self.p[:, :, self.down, self.walk_left] = .5 * (1 - p_walk)
 
         self.p[:, :, self.up, self.run_up] = p_run
         self.p[:, :, self.left, self.run_up] = .5 * (1 - p_run)
--- test_MDP.py
+++ test_MDP.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 from hw3cs561s2018 import MDP
 import numpy as np
+import numpy.testing as test
 
 
 class TestMDP(TestCase):
@@ -13,7 +14,7 @@ class TestMDP(TestCase):
         big = np.concatenate((m1, m2), axis=0)
         # print(big)
 
-        print(np.delete(big, (-1,-2), axis=0))
+        print(np.delete(big, (-1, -2), axis=0))
         # first_two = m2[-1:,...]
         # print(first_two)
         # print(np.concatenate((first_two, m2), axis=0))
@@ -23,4 +24,45 @@ class TestMDP(TestCase):
         m2 = np.array([0, 2.5, 2.9])
         max = np.maximum(m1, m2)
         print(m1 == max)
-       
+
+    def test_expand_value(self):
+        value = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+        value = value[:, :, np.newaxis, np.newaxis]
+        value = np.repeat(value, 4, 2)
+        value = np.repeat(value, 8, 3)
+        print(value.shape)
+
+        p = np.arange(3 * 3 * 4 * 8).reshape((3, 3, 4, 8))
+        result = np.multiply(value, p)
+        # print(p[:,:,0,0])
+        print(result[:, :, 0, 0])
+
+    def test_initial(self):
+        mdp = MDP(width=3, length=3, p_walk=0.7, p_run=0.6, reward_walk=1, reward_run=1, discount=0, exit_list=[((0, 2), 1), ((1, 2), -1)])
+        test.assert_array_equal(np.array([[0, 0, 1], [0, 0, -1], [0, 0, 0]]), mdp.value)
+
+        p_up_walk_up = np.squeeze(mdp.p[:, :, mdp.up, mdp.walk_up])
+        test.assert_array_equal(np.ones((3, 3)) * 0.7, p_up_walk_up)
+
+        p_down_walk_up = np.squeeze(mdp.p[:, :, mdp.down, mdp.walk_up])
+        test.assert_array_equal(np.zeros((3, 3)), p_down_walk_up)
+
+        p_left_walk_up = np.squeeze(mdp.p[:, :, mdp.left, mdp.walk_up])
+        test.assert_array_almost_equal(np.ones((3, 3)) * 0.15, p_left_walk_up)
+
+        p_right_walk_up = np.squeeze(mdp.p[:, :, mdp.right, mdp.walk_up])
+        test.assert_array_almost_equal(np.ones((3, 3)) * 0.15, p_right_walk_up)
+
+        for direction in (mdp.up, mdp.left, mdp.right, mdp.down):
+            for action in (mdp.walk_up, mdp.walk_left, mdp.walk_right, mdp.walk_down, mdp.run_up, mdp.run_left, mdp.run_right, mdp.run_down):
+                p = np.squeeze(mdp.p[:, :, direction, action])
+                if direction == action:  # walk_direction = direction
+                    test.assert_array_almost_equal(p, np.ones((3, 3)) * 0.7)
+                elif direction == action - 4:  # run_direction = direction
+                    test.assert_array_almost_equal(p, np.ones((3, 3)) * 0.6)
+                elif np.abs(direction - action) == 2 or np.abs(direction - action) == 6:  # opposite direction
+                    test.assert_array_almost_equal(p, np.zeros((3, 3)))
+                elif action < 4:  # walk
+                    test.assert_array_almost_equal(np.ones((3, 3)) * 0.15, p)
+                else: #run
+                    test.assert_array_almost_equal(np.ones((3, 3)) * 0.2, p)
